baf67acc76f7ebad4ec3f4f4b8a77b6599cea274
divinedavis <divinejdavis@gmail.com>
2015-05-27 22:12:32 -0400

    Added a new file, just to see how Netbeans operates

---
+++ NetBeansProjects/More Practice/src/more/practice/MorePractice.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package more.practice;
+
+/**
+ *
+ * @author divineDavis
+ */
+public class MorePractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte byt = 99;
+        short sht = 24513;
+        int integ = 456767133;
+        long lng = 1234578696;
+        
+        System.out.println("Byte: " + byt + "\nShort: " + sht + "\nInt: " + integ + "\nLong: " + lng);
+        
+    
+    }
+    
+}
