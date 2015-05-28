3809a9c80209d5ff74e77135919fafb7cac396a6
divinedavis <divinejdavis@gmail.com>
2015-05-27 22:30:21 -0400

    Adding arrays

--- NetBeansProjects/More Practice/src/more/practice/MorePractice.java
+++ NetBeansProjects/More Practice/src/more/practice/MorePractice.java
@@ -22,7 +22,25 @@ public class MorePractice {
         
         System.out.println("Byte: " + byt + "\nShort: " + sht + "\nInt: " + integ + "\nLong: " + lng);
         
-    
+        int weights [] = new int [100];
+        
+        weights [0] = 125;
+        weights [1] = 223;
+        weights [2] = 214;
+        weights [3] = 200;
+        weights [4] = 189;
+        weights [5] = 112;
+        weights [6] = 214;
+        weights [7] = 200;
+        weights [8] = 189;
+        weights [9] = 112;
+        
+        System.out.println(weights[0] + " " + weights[1] + " " + weights[2]);
+        
+        weights [4] = 111;
+        weights [5] = 123;
+        
+        System.out.println(weights[4] + " " + weights[5] + " " );
     }
     
 }
