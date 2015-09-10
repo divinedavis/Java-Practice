9ae5fe00a34fc3b5e2fcdcd4f558d809d988962a
divinedavis <divinejdavis@gmail.com>
2015-09-10 10:41:15 -0400

    Calculating inches to centimeters

--- NetBeansProjects/JavaIlluminated/src/JavaIlluminated.java
+++ NetBeansProjects/JavaIlluminated/src/JavaIlluminated.java
@@ -1,14 +1,32 @@
 
+import java.util.Scanner;
+
+
 /**
  *
  * @author divineDavis
  */
+
+
 public class JavaIlluminated {
 
     public static void main(String[] args) {
 
-    System.out.println("Programming is not a spectator sport");
-    
+//    System.out.println("Programming is not a spectator sport");
+//    
+        
+        final double CM_PER_INCH = 2.54;
+        int inches = 0;
+        double centimeter = inches * CM_PER_INCH;
+        
+        System.out.println("The initial value of centimeters is: " + centimeter);
+        
+        inches = 8;
+        
+        System.out.println("Now since the inches is 8, the centimeters is: " + centimeter);
+        
+        
+        
     }
     
 }
