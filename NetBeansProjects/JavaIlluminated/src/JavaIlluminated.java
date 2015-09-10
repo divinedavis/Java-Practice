0860e91025a9b6d931881a673237a6a6d63249e3
divinedavis <divinejdavis@gmail.com>
2015-09-10 11:33:28 -0400

    Finding the bug in why the conversion will always put the division to zero

--- NetBeansProjects/JavaIlluminated/src/JavaIlluminated.java
+++ NetBeansProjects/JavaIlluminated/src/JavaIlluminated.java
@@ -15,17 +15,24 @@ public class JavaIlluminated {
 //    System.out.println("Programming is not a spectator sport");
 //    
         
-        final double CM_PER_INCH = 2.54;
-        int inches = 0;
-        double centimeter = inches * CM_PER_INCH;
+//        final double CM_PER_INCH = 2.54;
+//        int inches = 0;
+//        double centimeter = inches * CM_PER_INCH;
+//        
+//        System.out.println("The initial value of centimeters is: " + centimeter);
+//        
+//        inches = 8;
+//        
+//        double newCentimeter = inches * CM_PER_INCH;
+//        System.out.println("Now since the inches is 8, the centimeters is: " + newCentimeter);
         
-        System.out.println("The initial value of centimeters is: " + centimeter);
+        double tempC, tempF;
         
-        inches = 8;
+        tempF = 80;
         
-        double newCentimeter = inches * CM_PER_INCH;
-        System.out.println("Now since the inches is 8, the centimeters is: " + newCentimeter);
+        double temp = (tempF - 32) * 5/9;
         
+        System.out.println("The temperature of 80ºF is the equivalent to " + temp + "ºC");
         
         
     }
