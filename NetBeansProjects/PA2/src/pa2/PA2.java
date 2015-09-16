d444bf8db656a7a646827ed22785ca8b9c2ae909
divinedavis <divinejdavis@gmail.com>
2015-09-16 19:28:58 -0400

    Doing problem 2 Almost done

--- NetBeansProjects/PA2/src/pa2/PA2.java
+++ NetBeansProjects/PA2/src/pa2/PA2.java
@@ -23,6 +23,14 @@ public class PA2 {
         String name = JOptionPane.showInputDialog("Please enter your first and last name");
         JOptionPane.showMessageDialog(null, "The length of your name is " + name.length() + " characters");
     
+        String birthYear = JOptionPane.showInputDialog(name + ", enter your birth year below");
+        int age = Integer.parseInt(birthYear);
+        int newAge = 2015 - age;
+        JOptionPane.showMessageDialog(null, "This year you will be " + ++newAge);
+        final double AVERAGE_LIFE_EXPECTANCY = 78.7;
+        double ageInPercentage = (newAge/AVERAGE_LIFE_EXPECTANCY);
+        JOptionPane.showMessageDialog(null, "The percentage of life you have left to live is: " + ageInPercentage);
+        
     }
     
 }
